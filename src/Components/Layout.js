2a61f22ab1a0665b00f19ae39f8c6298b0c03ff4
danilobatson <djbatson19@gmail.com>
2021-08-26 19:06:43 -0700

    working on margins

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -11,7 +11,6 @@ const drawerWidth = 240;
 const useStyles = makeStyles((theme) => {
 	return {
 		page: {
-			background: '#f9f9f9',
 			width: '100%',
 			padding: theme.spacing(3),
 		},
@@ -29,9 +28,7 @@ const useStyles = makeStyles((theme) => {
 		drawerPaper: {
 			width: drawerWidth,
 		},
-		active: {
-			background: '#f4f4f4',
-		},
+
 		title: {
 			padding: theme.spacing(2),
 		},
